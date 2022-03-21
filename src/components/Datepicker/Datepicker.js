2a11658b671e09fe1cf37ebbc098d1fcf2e88c6d
sujaymec <sujay.mec@gmail.com>
2022-03-21 22:53:17 +0530

    styles being applied

--- src/components/Datepicker/Datepicker.js
+++ src/components/Datepicker/Datepicker.js
@@ -2,20 +2,30 @@ import React from "react";
 import './datepicker.css'
 import { DatePicker } from "baseui/datepicker";
 
-// export const Datepicker = () => {
-//     return <>
-//         <DatePicker />
-//     </>
-// }
+import {BaseProvider, LightTheme} from 'baseui';
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+const engine = new Styletron();
 
 export default () => {
     const [value, setValue] = React.useState([new Date()]);
     return (
-      <DatePicker
-        value={value}
-        onChange={({ date }) =>
-          setValue(Array.isArray(date) ? date : [date])
-        }
-      />
+
+        <React.StrictMode>
+        <StyletronProvider value={engine}>
+          <BaseProvider theme={LightTheme}>
+            <DatePicker
+                value={value}
+                onChange={({ date }) =>
+                setValue(Array.isArray(date) ? date : [date])
+                }
+            />
+          </BaseProvider>
+        </StyletronProvider>
+      </React.StrictMode>
+
+
+
     );
   }
