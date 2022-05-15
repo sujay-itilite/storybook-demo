d53aa277e9ca4448e1464d46188f159cc4c88cc1
sujaymec <sujay.mec@gmail.com>
2022-05-15 23:01:13 +0530

    removing default button

--- src/components/Button-Itilite/Button.stories.jsx
+++ src/components/Button-Itilite/Button.stories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from './Button';
 
 export default {
-  title: 'Components/Button-Itilite',
+  title: 'Components/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Button } from './Button';
 
-export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-};
+// export default {
+//   title: 'Example/Button',
+//   component: Button,
+//   argTypes: {
+//     backgroundColor: { control: 'color' },
+//   },
+// };
 
 const Template = (args) => <Button {...args} />;
 
